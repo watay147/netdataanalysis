f2ae3aa257c4b05e82306493513e06fba0e1e384
wwz <449339387@qq.com>
2015-08-19 20:44:13 +0800

    数据迁移

---
+++ webpage/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('webpage', '0017_auto_20150813_1712'),
+        ('webpage', '0004_company_moncreditchange'),
+    ]
+
+    operations = [
+    ]
